5bc51e32d5a1c9d79c8acb7935270430e5d27615
kellimohr <kellimohr@gmail.com>
2013-01-14 08:58:46 -0800

    Changed for Listing 2.12

--- Chapter2/bacon/spec/bacon_spec.rb
+++ Chapter2/bacon/spec/bacon_spec.rb
@@ -4,4 +4,10 @@ describe Bacon do
   it "is edible" do
     Bacon.new.edible?.should be_true
   end
+
+  it "expired!" do
+  	bacon = Bacon.new
+  	bacon.expired!
+  	bacon.should be_expired
+  end
 end
